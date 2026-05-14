db02a8b4f8782634001847bd53f4303cd0ae7cec
mariangeles ielsich <ielsichmari@gmail.com>
2026-05-14 01:01:18 -0300

    tests actualizados validaciones

--- tests/test_saucedemo.py
+++ tests/test_saucedemo.py
@@ -20,21 +20,34 @@ def test_pre_entrega_saucedemo(driver):
     assert "inventory.html" in driver.current_url
     assert driver.find_element(By.CLASS_NAME, "app_logo").text == "Swag Labs"
     
-    # Verificacion de Catalogo (Titulo e Items)
+    # Verificacion de Catalogo (Criterios Minimos)
     assert driver.find_element(By.CLASS_NAME, "title").text == "Products"
     items = driver.find_elements(By.CLASS_NAME, "inventory_item")
-    assert len(items) > 0
+    assert len(items) > 0 # Valida presencia de productos
+    
+    # Extraer Nombre y Precio del primero (Criterio Minimo)
+    primer_nombre = driver.find_element(By.CLASS_NAME, "inventory_item_name").text
+    primer_precio = driver.find_element(By.CLASS_NAME, "inventory_item_price").text
+    print(f"\nProducto: {primer_nombre} | Precio: {primer_precio}")
+
+    # Validar elementos de interfaz (Menu y Filtros)
+    assert driver.find_element(By.ID, "react-burger-menu-btn").is_displayed()
+    assert driver.find_element(By.CLASS_NAME, "product_sort_container").is_displayed()
     
     # Flujo de Carrito (Agregar y Validar Badge)
     driver.find_element(By.ID, "add-to-cart-sauce-labs-backpack").click()
     badge = driver.find_element(By.CLASS_NAME, "shopping_cart_badge").text
     assert badge == "1"
     
-    # Validacion de Carrito (Titulo y Item)
-    driver.find_element(By.CLASS_NAME, "shopping_cart_link").click()
-    assert driver.find_element(By.CLASS_NAME, "title").text == "Your Cart"
+    # Validar Carrito y Producto Agregado
+    driver.find_element(By.CLASS_NAME, "shopping_cart_link").click()        
+    assert "cart.html" in driver.current_url
     cart_items = driver.find_elements(By.CLASS_NAME, "cart_item")
     assert len(cart_items) == 1
+    cart_nombre = driver.find_element(By.CLASS_NAME, "inventory_item_name").text
+    assert cart_nombre == primer_nombre
+    
+    
     
 
     
